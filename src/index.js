bbcdb95626733842abee1ea896063416e30a0b77
sunysty <seokjiseon@gmail.com>
2022-03-21 21:24:04 +0900

    📦  Package : fontawsome  설치

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@fortawesome/fontawesome-free/js/all.js';
 
 ReactDOM.render(
   <React.StrictMode>
