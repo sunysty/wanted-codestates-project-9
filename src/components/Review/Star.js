3896843835c7a632355fbb89729a1dca2a39170f
sunysty <seokjiseon@gmail.com>
2022-03-22 14:16:44 +0900

    💄 UI : Star 컴포넌트 추가

--- src/components/Review/Star.js
+++ src/components/Review/Star.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Star = () => {
-  return <div>Star</div>;
+  return (
+    <StarBox>
+      <img
+        src="https://i.balaan.io/mobile/img/icons/icon-star-black.png"
+        alt="fill"
+      />
+      <img
+        src="https://i.balaan.io/mobile/img/icons/icon-star-black.png"
+        alt="fill"
+      />
+      <img
+        src="https://i.balaan.io/mobile/img/icons/icon-star-black.png"
+        alt="fill"
+      />
+      <img
+        src="https://i.balaan.io/mobile/img/icons/icon-star-black.png"
+        alt="fill"
+      />
+      <img
+        src="https://i.balaan.io/mobile/img/icons/icon-star-gray.png"
+        alt="none"
+      />
+    </StarBox>
+  );
 };
 
 export default Star;
+
+const StarBox = styled.div`
+  img {
+    width: 30px;
+    height: 30px;
+  }
+`;
